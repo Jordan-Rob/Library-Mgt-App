ff956d6704f1cc68a99b0b8349d92fe4b008e22a
Jordan <jordanrob709@gmail.com>
2022-09-15 15:46:05 +0300

    created docTypes for member Article & membership | created custom scripts to run logic before doctypes are saved

---
+++ library_management/library_management/doctype/library_transaction/test_library_transaction.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, Jordan and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibraryTransaction(FrappeTestCase):
+	pass
